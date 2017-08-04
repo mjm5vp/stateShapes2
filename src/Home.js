802aaaf85a9054157f52e36a1c9b5a985f203670
Mark Moeller <MarkMoellerUVA@gmail.com>
2017-08-04 17:41:12 -0400

    ajax load

--- src/Home.js
+++ src/Home.js
@@ -22,12 +22,21 @@ class Home extends Component {
 
     let url = "https://state-shape-back-end.herokuapp.com/michigan/michiganSlides"
     $.ajax({
-      url,
-      method: "GET",
+      url: url,
+      type: "get",
       dataType: "json"
-    }).then((response) => {
-      console.log("ajax load")
-      })
+      // $.ajax takes an object as an argument with at least three key-value pairs...
+      // (1) The URL endpoint for the JSON object.
+      // (2) Type of HTTP request.
+      // (3) Datatype. Usually JSON.
+    }).done(() => {
+      console.log("Ajax request success!")
+    }).fail(() => {
+      console.log("Ajax request fails!")
+    }).always(() => {
+      console.log("This always happens regardless of successful ajax request or not.")
+    })
+
 
   }
 
